72fdaccf1cc78692f9cb9bb27ac350ab588438dd
Andrea Amatucci <amatucci.andrea@gmail.com>
2021-04-11 20:17:55 +0200

    string resources

--- app/src/main/java/com/amatucci/andrea/beers/ui/MonthYearPicker.kt
+++ app/src/main/java/com/amatucci/andrea/beers/ui/MonthYearPicker.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.amatucci.andrea.beers.R
 import com.amatucci.andrea.beers.databinding.DialogMonthYearPickerBinding
 import java.util.*
 
@@ -12,7 +13,6 @@ class MonthYearPickerDialog(private val date: Date = Date()) : DialogFragment() 
 
     companion object {
         private const val MIN_YEAR = 1990
-
     }
 
     private lateinit var binding: DialogMonthYearPickerBinding
@@ -31,8 +31,20 @@ class MonthYearPickerDialog(private val date: Date = Date()) : DialogFragment() 
             minValue = 0
             maxValue = 11
             value = cal.get(Calendar.MONTH)
-            displayedValues = arrayOf("Jan","Feb","Mar","Apr","May","June","July",
-                "Aug","Sep","Oct","Nov","Dec")
+            displayedValues = arrayOf(
+                getString(R.string.month_Jan),
+                getString(R.string.month_Feb),
+                getString(R.string.month_Mar),
+                getString(R.string.month_Apr),
+                getString(R.string.month_May),
+                getString(R.string.month_Jun),
+                getString(R.string.month_Jul),
+                getString(R.string.month_Aug),
+                getString(R.string.month_Sep),
+                getString(R.string.month_Oct),
+                getString(R.string.month_Nov),
+                getString(R.string.month_Dec)
+            )
         }
 
         binding.pickerYear.run {
@@ -43,10 +55,10 @@ class MonthYearPickerDialog(private val date: Date = Date()) : DialogFragment() 
         }
 
         return AlertDialog.Builder(requireContext())
-            .setTitle("Please Select View Month")
+            .setTitle(getString(R.string.dialogTitle))
             .setView(binding.root)
-            .setPositiveButton("Ok") { _, _ -> listener?.onDateSet(null, binding.pickerYear.value, binding.pickerMonth.value + 1, 1) }
-            .setNegativeButton("Cancel") { _, _ -> dialog?.cancel() }
+            .setPositiveButton(getString(R.string.ok)) { _, _ -> listener?.onDateSet(null, binding.pickerYear.value, binding.pickerMonth.value + 1, 1) }
+            .setNegativeButton(getString(R.string.cancel)) { _, _ -> dialog?.cancel() }
             .create()
     }
 }
