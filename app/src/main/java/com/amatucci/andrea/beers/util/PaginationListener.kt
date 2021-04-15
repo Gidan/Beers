3cc09b9163d132b4c9c74c7491096a2e958d76ad
Andrea Amatucci <amatucci.andrea@gmail.com>
2021-04-15 14:29:12 +0200

    minor change to List pagination. Simplified check on first visible list item.

--- app/src/main/java/com/amatucci/andrea/beers/util/PaginationListener.kt
+++ app/src/main/java/com/amatucci/andrea/beers/util/PaginationListener.kt
@@ -9,15 +9,12 @@ abstract class PaginationListener(private val layoutManager: LinearLayoutManager
     abstract fun loadNextPage()
     abstract fun isLoading() : Boolean
 
-    private val PAGE_SIZE = 25
-
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
-        val visibleItemCount: Int = layoutManager.childCount
         val totalItemCount: Int = layoutManager.itemCount
         val firstVisibleItemPosition: Int = layoutManager.findFirstVisibleItemPosition()
         if (!isLoading()) {
-            if (visibleItemCount + firstVisibleItemPosition >= totalItemCount && firstVisibleItemPosition >= 0 && totalItemCount >= PAGE_SIZE) {
+            if (firstVisibleItemPosition > totalItemCount - 6){
                 loadNextPage()
             }
         }
